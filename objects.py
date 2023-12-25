05164a9435298e0ef18cb804e149766629d64efb
Master Sam <105840191+ImMasterSam@users.noreply.github.com>
2023-12-26 00:32:35 +0800

    Add files via upload

--- objects.py
+++ objects.py
@@ -1,5 +1,10 @@
 import pygame
 
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.backends.backend_agg as agg
+import pylab
+
 pygame.init()
 
 class Button(pygame.sprite.Sprite):
@@ -50,7 +55,7 @@ class Dashboard(pygame.sprite.Sprite):
     y = 0
     width = 0
     height = 0
-    color_type = ["", "RED", "GREEN", "BLUE", "ORANGE", "YELLOW", "CYAN", "PINK"]
+    color_type = ["", "WHITE", "RED", "GREEN", "BLUE", "ORANGE", "YELLOW", "CYAN", "PINK"]
 
     font1 = pygame.font.Font("FFFFORWA.ttf", 30)
     font2 = pygame.font.Font("FFFFORWA.ttf", 20)
@@ -81,14 +86,17 @@ class Dashboard(pygame.sprite.Sprite):
 
         for i in range(1, teams+1):
             if(reinforcement[i]*reinforcement[0]):
-                pygame.draw.rect(self.image, color[i], [25, 80+50*i, 250, 35], 0)
-                self.team_num = self.font2.render(f"{self.color_type[i]}: {teams_num[i]}", True, (255, 255, 255))
+                pygame.draw.rect(self.image, color[i], [25, 100+50*i, 250, 35], 0)
+                if(i == 1):
+                    self.team_num = self.font2.render(f"{self.color_type[i]}: {int(teams_num[i])}", True, (0, 0, 0))
+                else:
+                    self.team_num = self.font2.render(f"{self.color_type[i]}: {int(teams_num[i])}", True, (255, 255, 255))
             else:
-                self.team_num = self.font2.render(f"{self.color_type[i]}: {teams_num[i]}", True, color[i])
-            self.image.blit(self.team_num, (30, 85+50*i))
+                self.team_num = self.font2.render(f"{self.color_type[i]}: {int(teams_num[i])}", True, color[i])
+            self.image.blit(self.team_num, (30, 105+50*i))
 
         if(show == 0):
-            if(self.rect.left>-400):
+            if(self.rect.left>-300):
                 self.x -= 10
                 self.rect.left = self.x
 
@@ -135,7 +143,7 @@ class Slider(pygame.sprite.Sprite):
 
         self.num = init
         self.num_word = self.font1.render(str(self.num), True, (255, 255, 255))
-        self.image.blit(self.num_word, (550, 10))
+        self.image.blit(self.num_word, (550-10*(self.num+self.min>=100), 10))
 
         self.button = pygame.Surface((width, width))
         self.button.fill((255, 50, 50))
@@ -157,7 +165,7 @@ class Slider(pygame.sprite.Sprite):
         self.image.blit(self.name, (0, 5))
 
         self.num_word = self.font1.render(str(int(self.num+self.min)), True, (255, 255, 255))
-        self.image.blit(self.num_word, (550, 10))
+        self.image.blit(self.num_word, (550-10*(self.num+self.min>=100), 10))
 
         return int(self.num+self.min)
 
@@ -173,19 +181,19 @@ class Checkbox(pygame.sprite.Sprite):
 
     font1 = pygame.font.Font("FFFFORWA.ttf", 30)
 
-    def __init__(self, name, x, y, width, height, scalex):
+    def __init__(self, name, x, y, width, height, scalex, init_on):
 
         pygame.sprite.Sprite.__init__(self)
 
         self.sprites = []
         for i in range(2):
-            pic = pygame.image.load(f"圖片\\{name}_{i}.png")
+            pic = pygame.image.load(f"圖片\\checkbox_{i}.png")
             self.sprites.append(pygame.transform.scale(pic, (width*scalex, height*scalex)).convert_alpha())
 
         self.image = pygame.Surface((600 ,50))
         self.image.fill((0, 0, 0))
 
-        self.box = self.sprites[1]
+        self.box = self.sprites[init_on]
         self.rect = self.image.get_rect()
         self.x = x
         self.y = y
@@ -193,7 +201,7 @@ class Checkbox(pygame.sprite.Sprite):
         self.rect.top = y
         self.width = width*scalex
         self.height = height*scalex
-        self.switch = 1
+        self.switch = init_on
 
         self.image.blit(self.box, (550, 5))
 
@@ -217,3 +225,129 @@ class Checkbox(pygame.sprite.Sprite):
         
         else:
             return self.switch
+
+
+class Checkbox_list(pygame.sprite.Sprite):
+
+    #變數
+    x = 0
+    y = 0
+    sprites = []
+    width = 0
+    height = 0
+
+    font1 = pygame.font.Font("FFFFORWA.ttf", 30)
+
+    def __init__(self, name, x, y, width, height, scalex, n):
+
+        pygame.sprite.Sprite.__init__(self)
+
+        self.sprites = [[]]
+        for i in range(1, n+1):
+            pic_on = pygame.image.load(f"圖片\\checkbox_team_{i}_0.png")
+            pic_off = pygame.image.load(f"圖片\\checkbox_team_{i}_1.png")
+            self.sprites.append([pygame.transform.scale(pic_on, (width*scalex, height*scalex)).convert_alpha(), pygame.transform.scale(pic_off, (width*scalex, height*scalex)).convert_alpha()])
+
+        self.image = pygame.Surface((600 ,140))
+        self.image.fill((0, 0, 0))
+
+        self.display = [[]]
+        for i in range(1, n+1):
+            self.display.append(self.sprites[i][1])
+        
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.left = x
+        self.rect.top = y
+        self.width = width*scalex
+        self.height = height*scalex
+        self.n = n
+        self.switchs = [x-x+1 for x in range(6)]
+
+        for i in range(1, n+1):
+            self.image.blit(self.sprites[i][self.switchs[i]], ((i-1)*135, 70))
+
+        self.name_word = self.font1.render(name, True, (255, 255, 255))
+        self.image.blit(self.name_word, (240, 10))
+
+
+    def draw(self):
+        for i in range(1, self.n+1):
+            self.image.blit(self.sprites[i][self.switchs[i]], ((i-1)*135, 70))
+
+    def press(self, pos):
+
+        if(pos[0]>=self.x and pos[0]<=self.x + 600) and (pos[1]>=self.y+70 and pos[1]<=self.y+self.height+70):
+
+            for i in range(1, self.n+1):
+
+                if(pos[0]>=self.x+((i-1)*135) and pos[0]<=self.x+self.width+((i-1)*135)):
+                    if(self.switchs[i]):
+                        self.switchs[i] = 0
+                        self.draw()
+                    else:
+                        self.switchs[i] = 1
+                        self.draw()
+
+        
+        return self.switchs
+
+
+class LineChart(pygame.sprite.Sprite):
+
+    def __init__(self, x, y, scale_x, scale_y, set_dpi):
+
+        pygame.sprite.Sprite.__init__(self)
+
+        self.figure = pylab.figure(figsize=[scale_x, scale_y], dpi = set_dpi, facecolor="black")
+        self.ax = self.figure.gca()
+        self.ax.set_facecolor("black")
+        self.ax.tick_params(axis='both', colors='white')
+        self.ax.grid(color='gray', linestyle='--', linewidth=0.5)
+        self.ax.set_title("Number of Cells", color="white")
+        self.ax.set_xlabel("Generations", color="white")
+
+        self.canvas = agg.FigureCanvasAgg(self.figure)
+        self.canvas.draw()
+        self.renderer = self.canvas.get_renderer()
+        self.raw_data = self.renderer.tostring_rgb()
+
+        self.canva_size = self.canvas.get_width_height()
+
+        self.image = pygame.image.fromstring(self.raw_data, self.canva_size, "RGB").convert_alpha()
+
+        self.rect = self.image.get_rect()
+        self.x = x
+        self.y = y
+        self.rect.left = x
+        self.rect.top = y
+
+
+    def update(self, datas, teams, colors):
+
+        for i in range(1, teams+1):
+            self.ax.plot(datas[i], color=(colors[i][0] / 255, colors[i][1] / 255, colors[i][2] / 255))
+        self.canvas.draw()
+        renderer = self.canvas.get_renderer()
+        #raw_data = renderer.tostring_rgb()
+        self.image = pygame.image.fromstring(renderer.tostring_rgb(), self.canva_size, "RGB").convert_alpha()
+
+
+    def clear(self, datas, teams, colors):
+
+        self.ax.cla()
+
+        self.ax.set_facecolor("black")
+        self.ax.tick_params(axis='both', colors='white')
+        self.ax.grid(color='gray', linestyle='--', linewidth=0.5)
+        self.ax.set_title("Number of Cells", color="white")
+        self.ax.set_xlabel("Generations", color="white")
+
+        for i in range(1, teams+1):
+            self.ax.plot(datas[i], color=(colors[i][0] / 255, colors[i][1] / 255, colors[i][2] / 255))
+        self.canvas.draw()
+        renderer = self.canvas.get_renderer()
+        #raw_data = renderer.tostring_rgb()
+        self.image = pygame.image.fromstring(renderer.tostring_rgb(), self.canva_size, "RGB").convert()
+
--- 康威人工生命.py
+++ 康威人工生命.py
@@ -10,16 +10,23 @@ playing = False
 game_status = 0
 tick = 0
 dashboard = 0
-teams = 2
-teams_num = [0, 0, 0, 0, 0, 0, 0, 0]
-reinforcement = [1, 0, 0, 0, 0, 0, 0, 0]
-size = 10
+teams = 5
+teams_num = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0])
+reinforcement = np.array([1, 0, 0, 0, 0, 0, 0, 0, 0])
+size = 2
 color = 0
 reset = 0
+speed = 100
+generation = 0
+show_plot = 0
+green_rate = 33
+need_refresh = 0
+selected = [x-x+1 for x in range(teams+1)]
+datas = [[x-x] for x in range(teams+1)]
 
 #主畫面
 pygame.init()
-screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN|pygame.HWSURFACE|pygame.DOUBLEBUF)
 width = screen.get_width()//size
 height = screen.get_height()//size
 pygame.display.set_caption("Conway's Game of Life")
@@ -29,6 +36,7 @@ bg = pygame.Surface(screen.get_size())
 bg.fill((0, 0, 0))
 clock = pygame.time.Clock()
 
+
 #雜七雜八的東西
 cover = pygame.Surface(screen.get_size(), pygame.SRCALPHA)
 cover.set_alpha(150)
@@ -36,7 +44,7 @@ cover.fill((20, 20, 20))
 cover = cover.convert_alpha()
 
 pencils = []
-for i in range(7):
+for i in range(8):
     pencil = pygame.image.load(f"圖片\\pencil_{i+1}.png")
     pencil = pygame.transform.scale(pencil, (50, 50))
     pencils.append(pencil)
@@ -46,9 +54,10 @@ font1 = pygame.font.Font("FFFFORWA.ttf", 40)
 q_word = font1.render("Conway's Game of Life", True, (255, 255, 255))
 
 #生命體的家
-colors = [(0, 0, 0), (255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 120, 40), (255, 255, 100), (100, 255, 255), (255, 100, 255)]
-list_of_life = []
-sur_cells = []
+colors = [(0, 0, 0), (255, 255, 255), (255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 120, 40), (255, 255, 100), (100, 255, 255), (255, 100, 255)]
+list_of_life = np.array([])
+sur_cells = np.array([])
+survive_time = np.array([])
 
 #物件都在這裡
 start_button = objects.Button("start_button", width*size/2-100, height*size/2-30, 50, 15, 2, 4)
@@ -62,10 +71,16 @@ quit_button = objects.Button("quit_button", width*size-100, 40, 15, 15, 2, 4)
 
 db = objects.Dashboard(-400, 30, 300, height*size-60)
 
-team_config = objects.Slider("Number of Team(s)", width*size/2-300, 130, 600, 50, 7, 1, teams)
-pixel_config = objects.Slider("Size of Cells", width*size/2-300, 280, 600, 50, 50, 1, 10)
+pixel_config = objects.Slider("Size of Cells", width*size/2-300, 130, 600, 50, 50, 1, size)
+fps_config = objects.Slider("Cycle Speed", width*size/2-300, 280, 600, 50, 100, 0, speed)
+green_config = objects.Slider("Green reproductionrate", width*size/2-300, 780, 600, 50, 100, 1, green_rate)
 
-reinforce_config = objects.Checkbox("Reinforcement", width*size/2-300, 430, 15, 15, 3)
+reinforce_config = objects.Checkbox("Reinforcement", width*size/2-300, 430, 15, 15, 3, reinforcement[0])
+show_plot_config = objects.Checkbox("Line Chart", width*size/2-300, 520, 15, 15, 3, show_plot)
+
+spawn_config = objects.Checkbox_list("Spawn", width*size/2-300, 620, 15, 15, 4, 5)
+
+line_chart = objects.LineChart(width*size-500,height*size-400, 5, 4, 100)
 
 #物件群組
 menu = pygame.sprite.Group()
@@ -83,28 +98,44 @@ edit = pygame.sprite.Group()
 edit.add(leave_button)
 edit.add(db)
 setting = pygame.sprite.Group()
-setting.add(team_config)
+setting.add(green_config)
 setting.add(pixel_config)
 setting.add(leave_button)
 setting.add(reinforce_config)
+setting.add(show_plot_config)
+setting.add(fps_config)
+setting.add(spawn_config)
+charts = pygame.sprite.Group()
+charts.add(line_chart)
 
 #清除全部
 def clear():
-    
-    global list_of_life
 
-    teams_num[1] = 0
-    teams_num[2] = 0
+    global list_of_life
+    global generation
+    global datas
+
+    generation = 0
 
     for i in range(height):
         for j in range(width):
             list_of_life[i][j] = 0
+            sur_cells[i][j] = 0
+            survive_time[i][j] = 0
             pygame.draw.rect(bg, (0, 0, 0), [j*size, i*size, size, size], 0)
 
         screen.blit(bg, (0, 0))
         menu.draw(screen)
 
         pygame.display.update()
+
+    datas = [[x-x] for x in range(6)]
+
+    for i in range(1, 6):
+        teams_num[i] = 0
+        datas[i][0] = 0
+
+    line_chart.clear(datas, teams, colors)
 
 
 #隨機生成
@@ -116,61 +147,89 @@ def random_reset():
     height = screen.get_height()//size
     global list_of_life
     global sur_cells
+    global survive_time
     list_of_life = np.array([[x-x for x in range(width)] for y in range(height)])
-    sur_cells = np.array([[[0, 0, 0, 0, 0, 0, 0, 0] for x in range(width)] for y in range(height)])
+    sur_cells = np.array([[[0, 0, 0, 0, 0, 0, 0, 0, 0] for x in range(width)] for y in range(height)])
+    survive_time = np.array([[x-x for x in range(width)] for y in range(height)])
     global teams_num
     global reset
+    global generation
+    generation = 0
     reset = 0
     bg.fill((0, 0, 0))
-    teams_num = [0, 0, 0, 0, 0, 0, 0, 0]
+    teams_num = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0])
+
+    global datas
+
+    spawn_nums = 0
+    spawn_teams = []
+    for i in range(1, 6):
+        if(selected[i]):
+            spawn_nums += 1
+            spawn_teams.append(i)
 
     for i in range(height):
         for j in range(width):
 
-            x = randint(0, 5)
-            if(x <2):
-                k = randint(1, teams)
-                list_of_life[i][j] = k
-                teams_num[k] += 1
+            x = randint(1, 100)
+            if(x <= spawn_nums*10):
+                k = randint(0, spawn_nums-1)
+                list_of_life[i][j] = spawn_teams[k]
+                teams_num[list_of_life[i][j]] += 1
             else:
                 list_of_life[i][j] = 0
 
             pygame.draw.rect(bg, colors[list_of_life[i][j]], [j*size, i*size, size, size], 0)
 
-            
         screen.blit(bg, (0, 0))
         menu.draw(screen)
 
         pygame.display.update()
 
-#更新生命體
-@numba.njit(fastmath = True)
-def update(sur_cells, list_of_life, temp, teams_num, teams, reinforcement, height, width):
+    datas = [[x-x] for x in range(teams+1)]
 
-    temp_num = [0, 0, 0, 0, 0, 0, 0, 0]
+    for i in range(1, teams+1):
+        datas[i][0] = teams_num[i]
+    line_chart.clear(datas, teams, colors) 
+
+#更新生命體
+@numba.jit(nopython = True, fastmath = True, looplift = True, cache=True, nogil = True)
+def update(sur_cells, list_of_life, survive_time, temp, teams_num, teams, reinforcement, generation, green_rate, height, width):
+
+    temp_num = np.zeros(teams+1)
+    explode = []
     
     for i in range(height):
         for j in range(width):
 
-            sur_cells[i][j] = [0, 0, 0, 0, 0, 0, 0, 0]    
+            sur_cells[i][j] = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0])  
             alive = 0
+            inflected = 0
 
             #上
             if(i>0 and list_of_life[i-1][j]):
                 alive += 1
                 sur_cells[i][j][list_of_life[i-1][j]] += 1
+                if(list_of_life[i-1][j] == 5):
+                    inflected = 1
             #下
             if(i<height-1 and list_of_life[i+1][j]):
                 alive += 1
                 sur_cells[i][j][list_of_life[i+1][j]] += 1
+                if(list_of_life[i+1][j] == 5):
+                    inflected = 1
             #左
             if(j>0 and list_of_life[i][j-1]):
                 alive += 1
                 sur_cells[i][j][list_of_life[i][j-1]] += 1
+                if(list_of_life[i][j-1] == 5):
+                    inflected = 1
             #右
             if(j<width-1 and list_of_life[i][j+1]):
                 alive += 1
                 sur_cells[i][j][list_of_life[i][j+1]] += 1
+                if(list_of_life[i][j+1] == 5):
+                    inflected = 1
 
             #左上
             if(j>0 and i>0 and list_of_life[i-1][j-1]):
@@ -193,8 +252,20 @@ def update(sur_cells, list_of_life, temp, teams_num, teams, reinforcement, heigh
             rival = 0
             #print(surround_num)
 
+            max_team = 0
+            max_sur = 0
+            max_rival = 0
+
+            reproduce_green = 1
+
             for k in range(1, teams+1):
-                if(teams_num[k] < max(teams_num)//2):
+                if(teams_num[k] > max_team):
+                    max_team = teams_num[k]
+                if(sur_cells[i][j][k] > max_sur):
+                    max_sur = sur_cells[i][j][k]
+
+            for k in range(1, teams+1):
+                if(teams_num[k] < max_team//2):
                     reinforcement[k] = 1
                 else:
                     reinforcement[k] = 0
@@ -202,38 +273,120 @@ def update(sur_cells, list_of_life, temp, teams_num, teams, reinforcement, heigh
                 if(list_of_life[i][j] == k):
                     mate += sur_cells[i][j][k]
                 else:
+                    if(sur_cells[i][j][k] > max_rival):
+                        max_rival = sur_cells[i][j][k]
                     rival += sur_cells[i][j][k]
+
+                if(k != 3 and sur_cells[i][j][k]>0):
+                    reproduce_green = 0
+
 
             #判斷存活or死亡
             if(list_of_life[i][j] > 0):
-                if(rival - mate > 1 + reinforcement[list_of_life[i][j]]*reinforcement[0]):
-                    temp[i][j] = 0
-                elif(alive<2):
-                    temp[i][j] = 0
-                elif(alive>3):
-                    temp[i][j] = 0
+
+                if(inflected and list_of_life[i][j] != 5):
+                    x = randint(1, 100)
+                    if(x<=25-15*(list_of_life[i][j] == 4)):
+                        temp[i][j] = 5
+                    elif(x<=40-40*(list_of_life[i][j] == 4)):
+                        temp[i][j] = 0
+                    else:
+                        temp[i][j] = list_of_life[i][j]
+
+                elif(list_of_life[i][j] == 3): #綠隊生存條件
+                    if(rival > 0):
+                        temp[i][j] = 0
+                    elif(alive<2):
+                        temp[i][j] = 0
+                    elif(alive>3):
+                        temp[i][j] = 0
+                    else:
+                        temp[i][j] = list_of_life[i][j]
+                
+                elif(list_of_life[i][j] == 4): #藍隊
+                    if(generation % 2):
+                        if(max_rival - mate > 1 + reinforcement[list_of_life[i][j]]*reinforcement[0] + 2*(list_of_life[i][j] == 4)):
+                            temp[i][j] = 0
+                        elif(alive<2):
+                            temp[i][j] = 0
+                        elif(alive>3):
+                            temp[i][j] = 0
+                        else:
+                            temp[i][j] = list_of_life[i][j]
+                    else:
+                        temp[i][j] = list_of_life[i][j]
+
                 else:
-                    temp[i][j] = list_of_life[i][j]
+                    if(max_rival - mate > 1 + reinforcement[list_of_life[i][j]]*reinforcement[0]):
+                        temp[i][j] = 0
+                    elif(alive<2):
+                        temp[i][j] = 0
+                    elif(alive>3):
+                        temp[i][j] = 0
+                    else:
+                        temp[i][j] = list_of_life[i][j]
 
 
             else:
                 index = []
                 for k in range(1, teams+1):
-                    if(sur_cells[i][j][k] == 3 and sur_cells[i][j][k] == max(sur_cells[i][j])):
+                    if(k == 3 and (sur_cells[i][j][3] >= 2) and reproduce_green): #綠隊繁殖
+                        x = randint(1, 100)
+                        if(x <= green_rate):
+                            index = [3]
+                            break
+                    elif(sur_cells[i][j][k] == 3 and sur_cells[i][j][k] == max_sur):
                         index.append(k)
                 #print(index)
 
                 if(len(index)==1):
-                    temp[i][j] = index[0]
+                    if(index[0] == 4): #籃隊繁殖
+                        if(generation % 2):
+                            temp[i][j] = 4
+                        else:
+                            temp[i][j] = 0
+                    
+                    else:
+                        temp[i][j] = index[0]
+
+            #確認紅色爆炸位置
+            if(temp[i][j] == 2 and survive_time[i][j] > 20 and survive_time[i][j] % 20 == 0):
+
+                explode.append((i, j))
                     
 
             if(temp[i][j]):
                 temp_num[temp[i][j]] += 1
+                if(temp[i][j] == list_of_life[i][j]):
+                    survive_time[i][j] += 1
+                else:
+                    survive_time[i][j] = 0
+            else:
+                survive_time[i][j] = 0
+
+    # 爆炸計算
+    for red in explode:
+
+        x = randint(1, 100)
+
+        if(x<=100):
+            for i in range(-2, 3):
+                for j in range(-2, 3):
+                    if(red[1]+i<0 or red[1]+i>width-1 or red[0]+j<0 or red[0]+j>height-1):
+                        continue
+                    if(temp[red[0]+j][red[1]+i] != 4):
+                        if(temp[red[0]+j][red[1]+i] and temp[red[0]+j][red[1]+i] != 2):
+                            teams_num[temp[red[0]+j][red[1]+i]] -= 1
+                            teams_num[2] += 1
+                        temp[red[0]+j][red[1]+i] = 2
+                        survive_time[red[0]+j][red[1]+i] = 0
+
 
     #teams_num = temp_num.copy()
     #list_of_life = temp.copy()
 
-    return temp_num, temp, sur_cells, reinforcement
+    return temp_num, temp, sur_cells, survive_time, reinforcement
+
 
 def draw():
 
@@ -279,6 +432,7 @@ while running:
             if start_button.detect(pos) and event.type == pygame.MOUSEBUTTONDOWN:
                 game_status = 1
             if setting_button.detect(pos) and event.type == pygame.MOUSEBUTTONDOWN:
+                need_refresh = 0
                 game_status = 3
             if clear_button.detect(pos) and event.type == pygame.MOUSEBUTTONDOWN:
                 clear()
@@ -291,10 +445,15 @@ while running:
     if(game_status == 1):
         db.update(dashboard, fps, colors, teams_num, teams, reinforcement)
         clock.tick(60)
-        tick = (tick+1)%6
-        if(tick == 0):
-            teams_num, list_of_life, sur_cells, reinforcement = update(sur_cells, list_of_life, np.array([[x-x for x in range(width)] for y in range(height)]), teams_num, teams, reinforcement, height, width)
+        tick += speed
+        if(tick >= 100):
+            tick -= 100
+            generation += 1
+            teams_num, list_of_life, sur_cells, survive_time, reinforcement = update(sur_cells, list_of_life, survive_time, np.array([[x-x for x in range(width)] for y in range(height)]), teams_num, teams, reinforcement, generation, green_rate, height, width)
+            for i in range(teams+1):
+                datas[i].append(teams_num[i])
             draw()
+            line_chart.update(datas, teams, colors)
         screen.blit(bg.convert(), (0, 0))
         ingame.draw(screen)
 
@@ -371,19 +530,30 @@ while running:
                 running = False
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
-                    random_reset()
+                    if(need_refresh):
+                        random_reset()
                     game_status = 0
             if event.type == pygame.MOUSEBUTTONDOWN:
                 reinforcement[0] = reinforce_config.press(pos)
+                show_plot = show_plot_config.press(pos)
+                selected = spawn_config.press(pos)
 
-            if team_config.detect(pos) and pygame.mouse.get_pressed()[0]:
-                teams = team_config.move(pos)
+            if green_config.detect(pos) and pygame.mouse.get_pressed()[0]:
+                green_rate = green_config.move(pos)
             if pixel_config.detect(pos) and pygame.mouse.get_pressed()[0]:
                 size = pixel_config.move(pos)
+                need_refresh = 1
+            if fps_config.detect(pos) and pygame.mouse.get_pressed()[0]:
+                speed = fps_config.move(pos)
             if leave_button.detect(pos) and event.type == pygame.MOUSEBUTTONDOWN:
-                random_reset()
+                if(need_refresh):
+                    random_reset()
                 game_status = 0
 
-    pygame.display.update()
+    if(show_plot):
+        charts.draw(screen)
+
+    pygame.display.flip()
+
 pygame.quit()
 
